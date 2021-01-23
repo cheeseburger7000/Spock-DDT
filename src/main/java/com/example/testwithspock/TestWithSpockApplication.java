578f1a424f921bd495f072d81ea448e20b7d67f4
Luca <shaohsiung@163.com>
2021-01-23 08:36:04 +0800

    add spock and groovy dependency

---
+++ src/main/java/com/example/testwithspock/TestWithSpockApplication.java
@@ -0,0 +1,13 @@
+package com.example.testwithspock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestWithSpockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestWithSpockApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testwithspock/TestWithSpockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testwithspock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestWithSpockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
